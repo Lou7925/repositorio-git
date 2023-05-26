39715d35a51270c8c545b7acfea9f316ede8d433
DiegoLecler <dfernandezlecler@gmail.com>
2023-05-26 16:03:52 -0300

    :rocket: Script para descargar NGNIX actualizado

---
+++ CLASE04/nginx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Script para la descargar e instalar la imagen de NGINX desde Docker Hub automaticamente
+
+echo "Descargamos la imagen de NGNIX 1.22.1 desde Docker Hub"
+
+docker pull nginx:1.22.1-alpine3.17-slim
